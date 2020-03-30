482e9ef1aa23d17344428b296b1643a37d7ec8a6
s1hagog <moshakalex@gmail.com>
2020-03-29 19:48:08 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,80 @@
+import _ from 'lodash';
+import Icon from './images/carbon.png';
+import './css/main.css';
+
+import * as Three from 'three';
+
+
+let scene, camera, renderer, cube;
+
+function init() {
+    scene = new Three.Scene();
+
+    camera = new Three.PerspectiveCamera(
+        75, 
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+
+    renderer = new Three.WebGLRenderer({
+        antialias: true
+    });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new Three.BoxGeometry( 2, 2, 2 );
+    const texture = new Three.TextureLoader().load(Icon);
+
+    const material = new Three.MeshBasicMaterial( {map: texture} );
+    cube = new Three.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+function onWindowResize () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
+animate();
+
+
+
+
+// function component() {
+//     const element = document.createElement('div');
+  
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+//     // Add the image to our existing div.
+//     const myIcon = new Image();
+//     myIcon.src = Icon;
+
+//     const newIcon = myIcon.cloneNode(false);
+
+//     element.appendChild(myIcon);
+    
+//     element.appendChild(newIcon);
+
+//     // element.appendChild();
+//     return element;
+//   }
+  
+//   document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'js/main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+      rules: [
+          {
+              test:  /\.(png|svg|jpg|gif)$/,
+              use: [
+                  'file-loader',
+              ]
+          },
+          {
+              test: /\.css$/,
+              use: [
+                  'style-loader',
+                  'css-loader',
+              ]
+          }
+      ],
+  },
+  plugins: [
+    new HtmlPlugin({
+        title: 'Three JS Fun',
+    }),
+  ],
+  devServer: {
+      contentBase: './dist',
+  }
+};
